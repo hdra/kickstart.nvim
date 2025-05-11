78246c43c2f3260ec459dd0f91916b6e8a1bd53d
Hendra <hendra2392@gmail.com>
2025-05-11 10:04:28 +0700

    Move elixir plug

--- init.lua
+++ init.lua
@@ -109,7 +109,6 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.wrap = false
 
-
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
@@ -908,36 +907,6 @@ require('lazy').setup({
     --   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     -- end,
-  },
-
-  {
-    'elixir-tools/elixir-tools.nvim',
-    version = '*',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local elixir = require 'elixir'
-      local elixirls = require 'elixir.elixirls'
-
-      elixir.setup {
-        nextls = { enable = true },
-        credo = {},
-        elixirls = {
-          enable = true,
-          settings = elixirls.settings {
-            dialyzerEnabled = false,
-            enableTestLenses = false,
-          },
-          on_attach = function(client, bufnr)
-            vim.keymap.set('n', '<space>fp', ':ElixirFromPipe<cr>', { buffer = true, noremap = true })
-            vim.keymap.set('n', '<space>tp', ':ElixirToPipe<cr>', { buffer = true, noremap = true })
-            vim.keymap.set('v', '<space>em', ':ElixirExpandMacro<cr>', { buffer = true, noremap = true })
-          end,
-        },
-      }
-    end,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
   },
 
   {
