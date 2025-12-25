1936a729f489d16950012504f9173c1a81a69698
Hendra <hendra2392@gmail.com>
2025-12-25 09:21:45 +0700

    update term

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -1,8 +1,12 @@
 return {
   'vim-test/vim-test',
   init = function()
-    vim.g['test#strategy'] = 'neovim_sticky'
-    vim.g['test#neovim#term_position'] = 'vert'
+    -- vim.g['test#strategy'] = 'neovim_sticky'
+    -- vim.g['test#neovim#term_position'] = 'vert'
+    vim.g['test#strategy'] = 'floaterm'
+
+    -- press ctrl-o to go back to terminal mode so can scroll through the test result
+    vim.keymap.set('t', '<C-o>', '<C-\\><C-n>', { noremap = true })
     vim.keymap.set('n', '<leader>tn', function()
       vim.cmd 'TestNearest'
     end, { desc = '[T]est [n]earest' })
